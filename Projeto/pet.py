ada7e3a413bdd7ef2a3b758132e3d6dd5c095f44
Guilherme Chaves <53453346+gbschaves@users.noreply.github.com>
2022-11-10 22:27:26 -0300

    Add files via upload

---
+++ Projeto/pet.py
@@ -0,0 +1,8 @@
+#Cadastro de pet
+class pet:
+    def __init__(self, nomePet, dataNascimento, CPF, obersvacoes):
+        _nome = nomePet
+        _dataNascimento = dataNascimento
+        _cpf = CPF
+        _observacoes = observacoes
+        

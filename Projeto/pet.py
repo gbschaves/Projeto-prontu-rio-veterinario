bc310004338ba9f821fc43e1c9f7038ad1a4a7ca
Guilherme Chaves <53453346+gbschaves@users.noreply.github.com>
2022-11-24 19:48:38 -0300

    Delete Projeto directory

--- Projeto/pet.py
+++
@@ -1,8 +0,0 @@
-#Cadastro de pet
-class pet:
-    def __init__(self, nomePet, dataNascimento, CPF, obersvacoes):
-        _nome = nomePet
-        _dataNascimento = dataNascimento
-        _cpf = CPF
-        _observacoes = observacoes
-        
--- Projeto/tutor.py
+++
@@ -1,8 +0,0 @@
-#Cadastro de tutor
-class Tutor:
-    def __init__(self, nome, CPF, endereco, bairro, dataNascimento):
-        _nome = nome
-        _cpf = CPF
-        _endereco = endereco
-        _bairro = bairro
-        _dataNascimento = dataNascimento
